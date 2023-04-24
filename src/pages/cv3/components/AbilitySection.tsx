b94593e6ec0f314b67daaa0926467c203d86ecd8
congphamtienthanh <congphamtienthanh@gmail.com>
2023-04-24 16:06:14 +0700

    up and down

--- src/pages/cv3/components/AbilitySection.tsx
+++ src/pages/cv3/components/AbilitySection.tsx
@@ -1,21 +1,11 @@
-import DescriptionIcon from '@mui/icons-material/Description';
-import { Avatar, Grid, Stack, Typography } from "@mui/material";
-import PersonIcon from "@mui/icons-material/Person";
-import SchoolIcon from "@mui/icons-material/School";
-import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
-import React from "react";
+import { Grid, Stack, Typography } from "@mui/material";
 import Section from "./Section";
 // import { ModelStudent } from '@pdt/models/ctsv.student';
 import { ECGCandidateCV } from "../../../models/ecg.candidate";
-import ContentListTime from "./ContentListTime";
+import ContentTime from "./ContentTime";
 
 const AbilitySection = ({ data }: { data?: ECGCandidateCV | null }) => {
-  const styleIcon = {
-    bgcolor: "transparent",
-    width: "30px",
-    height: "30px",
-    color: "#fff",
-  };
+
   return (
     <Grid item xs={8} bgcolor="transparent" position="relative">
       <Stack ml={4} py={2} mr={1} mb={8}>
@@ -36,6 +26,7 @@ const AbilitySection = ({ data }: { data?: ECGCandidateCV | null }) => {
       <Stack ml={4} py={2} mr={1}>
         <Section
           title="about me"
+          section={2}
           content={
             <Stack spacing={1} mt={2}>
               <Typography fontSize={13}>{data?.intro}</Typography>
@@ -44,59 +35,8 @@ const AbilitySection = ({ data }: { data?: ECGCandidateCV | null }) => {
         />
       </Stack>
       <Stack spacing={3} ml={4} py={2} mr={1}>
-        <Section
-          title="Education"
-          content={
-            <Stack spacing={2}>
-              {data?.education?.map((item, index) => (
-                <ContentListTime key={`education-` + index} {...item} />
-              ))}
-            </Stack>
-          }
-        />
-        <Section
-          title="Experience"
-          content={
-            <Stack spacing={2}>
-              {data?.experience?.map((item, index) => (
-                <ContentListTime key={`education-` + index} {...item} />
-              ))}
-            </Stack>
-          }
-        />
-        <Section
-          title={'reference'}
-          icon={
-            <Avatar sx={{ ...styleIcon }}>
-              <DescriptionIcon />
-            </Avatar>
-          }
-          content={
-            <Grid container columnSpacing={10} rowSpacing={2}>
-              <Grid item xs={6} pl={0}>
-                <Stack>
-                  <Typography fontSize={15} fontWeight={600}>
-                    Nguyễn Hoàng Long
-                  </Typography>
-                  <Typography fontSize={12}>Director: Nguyen Hoang Long</Typography>
-                  <Typography fontSize={12}>Phone: 0123456789</Typography>
-                  <Typography fontSize={12}>Email: Long@gmail.com</Typography>
-                </Stack>
-              </Grid>
-
-              <Grid item xs={6}>
-                <Stack>
-                  <Typography fontSize={15} fontWeight={600}>
-                    Nguyễn Hoàng Long
-                  </Typography>
-                  <Typography fontSize={12}>Director: Nguyen Hoang Long</Typography>
-                  <Typography fontSize={12}>Phone: 0123456789</Typography>
-                  <Typography fontSize={12}>Email: Long@gmail.com</Typography>
-                </Stack>
-              </Grid>
-            </Grid>
-          }
-        />
+        <ContentTime title="education" data={data}/>
+        <ContentTime title="experience" data={data}/>
       </Stack>
     </Grid>
   );
--- src/pages/cv3/components/Section.tsx
+++ src/pages/cv3/components/Section.tsx
@@ -1,34 +1,86 @@
-import { Box, Stack, Typography } from '@mui/material';
-import React, { ReactNode } from 'react';
+import { Box, Stack, Typography } from "@mui/material";
+import React, { ReactNode } from "react";
 
 const Section = ({
   title,
   content,
   icon,
-  dividerColor = '#208BB6'
+  section = 1,
 }: {
   icon?: ReactNode;
   title: ReactNode;
   content: ReactNode;
-  dividerColor?: string;
+  section?: number;
 }) => {
   return (
     <Box>
       <Stack
         direction="row"
         spacing={1}
-        justifyContent={!icon ? 'center' : 'initial'}
+        justifyContent={section === 1 ? "center" : "flex-start"}
         alignItems="center"
         mb="10px"
       >
         {icon}
-        <Typography textTransform="uppercase" fontSize={20} fontWeight={500} letterSpacing="1px">
-          {title}
-        </Typography>
+        {section === 1 ? (
+          <Typography
+            textTransform="uppercase"
+            fontSize={20}
+            fontWeight={500}
+            letterSpacing="1px"
+          >
+            {title}
+          </Typography>
+        ) : (
+          <>
+            <div
+              style={{
+                backgroundColor: "#274256",
+                width: "200px",
+              }}
+            >
+              <Typography
+                textTransform="uppercase"
+                fontSize={20}
+                fontWeight={500}
+                letterSpacing="1px"
+                style={{ color: "#fff", padding: "10px 0 10px 20px" }}
+              >
+                {title}
+              </Typography>
+            </div>
+            <div
+              style={{
+                width: "150px",
+                height: "50px",
+                margin: 0,
+                position: "relative",
+              }}
+            >
+              <div
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  backgroundColor: "#01B2E4",
+                  clipPath: "ellipse(100% 99% at 0% 99%)",
+                }}
+              ></div>
+              <div
+                style={{
+                  width: "80%",
+                  height: "100%",
+                  backgroundColor: "#274256",
+                  clipPath: "ellipse(100% 99% at 0% 99%)",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  zIndex: 1,
+                }}
+              ></div>
+            </div>
+          </>
+        )}
       </Stack>
-
-      {/* <Divider sx={{ backgroundColor: dividerColor, height: 1.5, my: 1 }} /> */}
-
       {content}
     </Box>
   );
